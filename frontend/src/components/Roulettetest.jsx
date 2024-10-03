5e2aac93147bfd9ebd0d661ba1b6989f796a6839
ddiieeggoo b <zkpgmi@gmail.com>
2024-10-03 13:21:39 +0200

    save

--- frontend/src/components/Roulettetest.jsx
+++ frontend/src/components/Roulettetest.jsx
@@ -1,57 +1,50 @@
-const { address } = useAccount();
-const [events, setEvents] = useState([])
+  const { address } = useAccount();
+  const [events, setEvents] = useState([]);
 
-// On récupère la balance ici
-const { data: lastWinningNumber, error, isPending, refetch } = useReadContract({
-  address: contractAddress,
-  abi: contractAbi,
-  functionName: 'getRequestStatus',
-})
+  const getEvents = async() => {
+    const frontendWinningNumber = await publicClient.getLogs({
+      address: contractAddress,
+      event: parseAbiItem('event RequestFulfilled(uint256 requestId, uint256[] randomWords, uint256 winninNumber)'),
+      // du premier bloc
+      fromBlock: 12724510n,
+      // jusqu'au dernier
+      toBlock: 'latest' // Pas besoin valeur par défaut
+    })
 
-const getEvents = async() => {
-  const depositEvents = await publicClient.getLogs({
-    address: contractAddress,
-    event: parseAbiItem('event etherDeposited(address indexed account, uint amount)'),
-    // du premier bloc
-    fromBlock: 0n,
-    // jusqu'au dernier
-    toBlock: 'latest' // Pas besoin valeur par défaut
-  })
+    const frontendTotalWinAmount = await publicClient.getLogs({
+      address: contractAddress,
+      event: parseAbiItem('event WinAmountCalculated(uint256 totalWinAmount)'),
+      // du premier bloc
+      fromBlock: 12724510n,
+      // jusqu'au dernier
+      toBlock: 'latest' // Pas besoin valeur par défaut
+    })
 
-  const withdrawEvents = await publicClient.getLogs({
-    address: contractAddress,
-    event: parseAbiItem('event etherWithdrawed(address indexed account, uint amount)'),
-    // du premier bloc
-    fromBlock: 0n,
-    // jusqu'au dernier
-    toBlock: 'latest' // Pas besoin valeur par défaut
-  })
+    const combinedEvents = frontendWinningNumber.map((event) => ({
+      type: 'Winning Number',
+      address: event.args.account,
+      winningNumber: event.args.winningNumber,
+      blockNumber: Number(event.blockNumber)
+    })).concat(frontendTotalWinAmount.map((event) => ({
+      type: 'Total Win Amount',
+      address: event.args.account,
+      totalWinAmount: event.args.totalWinAmount,
+      blockNumber: Number(event.blockNumber)
+    })))
 
-  const combinedEvents = depositEvents.map((event) => ({
-    type: 'Deposit',
-    address: event.args.account,
-    amount: event.args.amount,
-    blockNumber: Number(event.blockNumber)
-  })).concat(withdrawEvents.map((event) => ({
-    type: 'Withdraw',
-    address: event.args.account,
-    amount: event.args.amount,
-    blockNumber: Number(event.blockNumber)
-  })))
+    // sort by value
+    combinedEvents.sort(function (a, b) {
+      return b.blockNumber - a.blockNumber;
+    });
 
-  // sort by value
-  combinedEvents.sort(function (a, b) {
-    return b.blockNumber - a.blockNumber;
-  });
-
-  setEvents(combinedEvents)
-}
-
-useEffect(() => {
-  const getAllEvents = async() => {
-    if(address !== 'undefined') {
-      await getEvents();
-    }
+    setEvents(combinedEvents)
   }
-  getAllEvents();
-}, [address])
+
+  useEffect(() => {
+    const getAllEvents = async() => {
+      if(address !== 'undefined') {
+        await getEvents();
+      }
+    }
+    getAllEvents();
+  }, [address])
